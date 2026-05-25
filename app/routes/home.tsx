96c6e616d2f9199d0241fc6f27f304a820dfba9b
helloworld3200 <helloworld32.tech@gmail.com>
2026-05-25 23:32:26 +0100

    update app.css, home.tsx, package.json and pnpm-lock.yaml

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,15 @@
 import type { Route } from "./+types/home";
 
+// Maybe make it so that each element fades in?
+
 function Name() {
-  return (<h1 className="text-9xl font-semibold text-center tracking-normal 
+  // the idea here is
+  // a very nice looking name with features:
+  // 1. slowly shifting gradient
+  // 2. shimmer effect on hover that changes how that bg gradient moves around
+  // 3. overall "frosted glass" look achieved through edge outline which is just a slightly translucent + blurred background
+
+  return (<h1 className="text-9xl font-semibold text-center tracking-tight 
   bg-linear-20 from-blue-500 via-purple-500 to-blue-500 
   bg-size-[200%] text-transparent bg-clip-text animate-gradient">
     helloworld3200
